42b1398a8b7002a3971232fd2017a040bc8dae69
danielpeteani <danielpeteani@outlook.com>
2024-04-08 13:11:57 +0200

    docker-compose.yml - sprememba dodan index.html - spletna stran

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,20 +2,24 @@
 <html lang="it">
 <head>
     <meta charset="UTF-8">
-    <title>Produzione audiovisiva della comunità slovena in Italia</title>
+    <title>Oltre il confine</title>
     <link rel="stylesheet" href={{ asset('css/styleFilm.css') }}>
 </head>
 <body>
-<h1>Produzione audiovisiva della comunità slovena in Italia</h1>
-<p>Il database è dedicato alla produzione audiovisiva della comunità slovena in Italia. All'interno sono presenti i prodotti
-    audiovisivi di registi e produttori che compongono la comunità slovena in Italia.</p>
-<strong>ENG</strong>
-<p>
-
+<h1 class="titolo-iniziale">Registi e produzioni cinematografiche della comunità slovena in Italia</h1>
+<p class="descrizione-iniziale">
+    Lo scopo di questa ricerca è quello di esaminare e catalogare la produzione audiovisiva della comunità slovena in Italia,
+    offrendo uno sguardo inedito su un vasto patrimonio riunito, per la prima volta, in un insieme organico.
 </p>
 <strong>SLO</strong>
-<p>
-
+<p class="descrizione-iniziale">
+Namen te raziskave je preučiti in katalogizirati avdiovizualno produkcijo slovenske skupnosti v Italiji, in ponuditi pogled
+     na obsežno dediščino, ki je prvič združena v enovito celoto.
+</p>
+<strong>ENG</strong>
+<p class="descrizione-iniziale">
+The purpuse of this research is to examine and catalogue the audiovisual production of the Slovene community in Italy,
+    offering an unprecedented look at a vast heritage brought together, for the first time, in an organic whole.
 </p>
 <a href="/films" style="color: #000; text-decoration: none; padding: 12px">
     <div class="bottone">
